c14dc61fd53c85510eb4a93b07b2a2905d50f085
ujjwal <ujjwal.12100544@lpu.in>
2025-04-15 02:15:45 +0530

    edit

--- backend/modal/config/db.js
+++ backend/modal/config/db.js
@@ -2,20 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://192.168.33.131:27017/examts",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000,
-        connectTimeoutMS: 10000,
-      }
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `mongodb connected:${conn.connection.host}`
     );
-    console.log(`MongoDB connected to 192.168.170.83:27017`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1);
+    console.log(`Error:${error.message}`);
+    process.exit();
   }
 };
-
 module.exports = connectDB;
